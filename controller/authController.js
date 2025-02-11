19b6b2e64e236efa6f1019ae543358b8eeec2429
BhumikaKC <bhumikakc09@gmail.com>
2025-02-11 22:59:19 +0545

    Update authentication logic and environment configuration

--- controller/authController.js
+++ controller/authController.js
@@ -7,23 +7,6 @@ const bcrypt = require('bcrypt'); // Assuming you're using bcrypt for password h
 const nodemailer = require('nodemailer');
 const crypto = require('crypto'); // To generate a secure OTP
 
-
-// const registerStudent = async (req, res) => {
-//     const { collegeId, name, email, password, role } = req.body;
-
-//     try {
-//         const result = await UserService.registerStudent(collegeId, name, email, password, role);
-//         res.status(201).json(result);
-//     } catch (error) {
-//         console.error(error); // Log the error to the console
-//         // Check for the specific error message
-//         if (error.message === 'Student ID or email not found in college database.') {
-//             return res.status(400).json({ message: error.message }); // Send specific error message
-//         }
-//         // Handle other server errors
-//         res.status(500).json({ message: 'Server error', error: error.message }); // Return the error message
-//     }
-// };
 const registerStudent = async (req, res) => {
     const { collegeId, name, email, password, role } = req.body;
 
