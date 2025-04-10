96025065458801c8c13073f71ae2c7b869bbe5b7
BhumikaKC <bhumikakc09@gmail.com>
2025-04-10 23:10:52 +0545

    Incresed the time of token

--- services/userService.js
+++ services/userService.js
@@ -47,7 +47,7 @@ const loginUser = async (identifier, password) => {
     const token = jwt.sign(
         { user_id: user.user_id, role: user.role },
         process.env.JWT_SECRET, // Secret key from your .env file
-        { expiresIn: '1h' } // Set token expiration (optional)
+        { expiresIn: '24h' } // Set token expiration (optional)
     );
 
     const { password: _, ...userData } = user.dataValues;
